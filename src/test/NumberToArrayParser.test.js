d2c752df554ef0267eefd60d0bef106b8cba1b06
Gransly <bob031299@gmail.com>
2021-07-12 01:51:25 +1000

    fix

--- src/test/NumberToArrayParser.test.js
+++ src/test/NumberToArrayParser.test.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var NumberToArrayParser_1 = require("../main/NumberToArrayParser");
-describe('numbToArray', function () {
+describe('numb1234ToArray', function () {
+    //Arrange
     var numbers = 1234;
+    //Assert
     var expected = [1, 2, 3, 4];
+    //Act
     test('', function () {
         expect(NumberToArrayParser_1.NumberToArrayParser.numberToArray(numbers)).toEqual(expected);
     });
--- src/test/NumberToArrayParser.test.ts
+++ src/test/NumberToArrayParser.test.ts
@@ -1,9 +1,13 @@
 import {NumberToArrayParser} from "../main/NumberToArrayParser";
 
-describe('numbToArray', () =>{
+describe('numb1234ToArray', () =>{
+    //Arrange
     const numbers : number = 1234;
+    //Assert
     const expected :number[] = [1,2,3,4];
+    //Act
     test('', ()=>{
         expect(NumberToArrayParser.numberToArray(numbers)).toEqual(expected);
     })
 })
+
